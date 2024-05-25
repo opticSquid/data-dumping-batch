61b9f5ec54372347e5b8ddb1989a5871e66c6b21
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-05-25 15:31:28 +0530

    initial commit

---
+++ src/main/java/com/example/batch/demo_data_migration/DemoDataMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.batch.demo_data_migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDataMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDataMigrationApplication.class, args);
+	}
+
+}
